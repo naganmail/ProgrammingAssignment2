e65e6a66df636c0d80a6867a6747083c626ecad5
naganmail <naganmail@gmail.com>
2014-04-12 19:40:22 +0200

    Cachedmatrix initial version
    both functions implemented, tested and commented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions solves quadratic matrices using simple caching
 
-## Write a short comment describing this function
-
+## makeCacheMatrix - constructs cached matrix object
+##                  'x' is a quadratic matrix
 makeCacheMatrix <- function(x = matrix()) {
+  # initialize an empty cache
+  inverse <- NULL
+  
+  # define data access functions (getters and setters)
+    # updates the stored matrix and resets the cache
+    set <- function(y) {
+      x <<- y
+      inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
 
+  # make function names available for execution
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve: This function computes the inverse of the special "matrix" returned by 
+##             makeCacheMatrix above. If the inverse has already been calculated 
+##             (and the matrix has not changed), then the cachesolve retrieves 
+##             the inverse from the cache.
+##            'x' cached matrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get current inverse matrix value
+  inverse <- x$getinverse()
+  
+  # once the inverse matrix is in cache just return its content
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  # if cache is empty get the matrix data, solve it for inverse, store in cache 
+  # and return the result 
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  inverse
 }
